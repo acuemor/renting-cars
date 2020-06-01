e6fe75e500b66111e02e15a7ed0c95ecaef98493
Abel Cuevas <abel.cuevas.contractor@bbva.com>
2020-06-01 12:19:11 +0200

    feat(): Add components [GLOMOPE-17068]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { CarListComponent } from './car/car-list/car-list.component';
+import { CarDetailComponent } from './car/car-detail/car-detail.component';
+import { CarItemComponent } from './car/car-item/car-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    CarListComponent,
+    CarDetailComponent,
+    CarItemComponent
   ],
   imports: [
     BrowserModule,
